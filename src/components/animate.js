67ff504f96e4726dae1ccbdcf0bc890ca2dcfab8
Daniel Harrington <novus@hotmail.com>
2022-03-16 01:42:47 -0400

    Merge branch 'master' of https://github.com/Daniel-Harrington/Portfolio-Website

--- src/components/animate.js
+++ src/components/animate.js
@@ -1,36 +1,36 @@
 import * as THREE from 'three';
+import { skills } from './init';
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min';
-import { render,renderer } from './renderer';
-import {ContentBoxes,wave} from './objects'
-import { stats,rendererStats } from './debug';
-
+import { render, renderer } from './renderer';
+import { ContentBoxes, skybox, wave } from './objects'
+import { stats, rendererStats } from './debug';
+import { water } from './objects';
 const clock = new THREE.Clock()
 
+let i=0
 const tick = () => {
-
     const elapsedTime = clock.getElapsedTime()
-
     // Update objects
-   
+    
+    water.material.uniforms['time'].value = elapsedTime *0.01;
+    // console.log('time uniform:', water.material.uniforms['time'].value )
     // Content Boxes Floating Animation
     ContentBoxes.forEach(box => {
         box.mesh.position.y += -0.25 * Math.sin(elapsedTime)
         box.mesh.needsUpdate = true
     });
     //Wave Animation
-
-    const pos = wave.geometry.attributes.position;
+    let pos = wave.geometry.attributes.position;
     const vec3 = new THREE.Vector3(); // re-use
     for (let i = 0, l = pos.count; i < l; i++) {
-        
+
         //Gets the x,y,z values of vertex at current position
         vec3.fromBufferAttribute(pos, i);
 
         //Applies Sinusoidal Function To Z Values of Plane Vertices
-        const z = Math.sin((vec3.y / -200 + elapsedTime)) * 100;
+        let z = Math.sin((vec3.y / -200 + elapsedTime)) * 100;
         pos.setZ(i, z);
     }
-
     pos.needsUpdate = true
     
     //Tween updates
@@ -45,7 +45,6 @@ const tick = () => {
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
-
 }
 
-export {tick}
+export { tick }
