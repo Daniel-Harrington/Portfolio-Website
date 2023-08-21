2b68ce816ce8a018e466dbb60cd13c6c4be8b74e
Daniel Harrington <novus@hotmail.com>
2023-08-21 12:51:28 -0400

    Changed out css variables
    CSS variables were causing issues so I just hard coded the mobile and desktop versions

--- src/components/lights.js
+++ src/components/lights.js
@@ -1,6 +1,5 @@
 import { scene } from './globals'
 import { AmbientLight, HemisphereLight, DirectionalLight,DirectionalLightHelper } from 'three';
-import * as dat from 'dat.gui';
 // Lights
 let sun;
 
@@ -17,20 +16,7 @@ function initLights() {
     sun.position.y =  3000
     sun.position.z =  0
     scene.add(sun)
-    if (process.env.NODE_ENV === 'development') {
-    const gui = new dat.GUI();   
-    const light1 = gui.addFolder('Light 1')
-    light1.add(sun.position, 'y').min(-6000).max(6000).step(1)
-    light1.add(sun.position, 'x').min(-6000).max(6000).step(1)
-    light1.add(sun.position, 'z').min(-6000).max(6000).step(1)
-    light1.add(sun,'intensity').min(0).max(10).step(0.01)
-    
-const sunHelper = new DirectionalLightHelper(sun,2)
-scene.add(sunHelper)
-    // sun = new HemisphereLight()
-    // sun.intensity = 0.2
-    // scene.add(sun)
-    }
+
 }
 
 export { initLights, sun };
